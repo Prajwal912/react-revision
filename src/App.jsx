6fc4a29ecd13c7115a1ce3103f18fa4918467cfb
prajwal <prajwalshah912@gmail.com>
2024-04-18 22:12:05 +0530

    revision

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import "./App.css";
 
 function App() {
   const images = [
-    { src: "images/default_img.png" },
     { src: "https://placehold.co/200" },
     { src: "https://placehold.co/200x200/000000/FFFFFF.png" },
     { src: "https://placehold.co/200/orange/white" },
     { src: "https://placehold.co/200/blue/white" },
   ]
-
+  const selectedImage = images[2];
   return (
     <>
-      <div className="flex items-center justify-center gap-2">
+      <div className="flex items-center justify-center gap-2 mt-3">
         {images.map((img) => (
-          <img className="rounded-full"
+          <img className="rounded-full border-2 border-red-800 shadow-md"
             src={img.src}
             key={img.src}
             alt=""
